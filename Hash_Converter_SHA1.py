a72b6ae381d06e3bb6e6eee37815c80362e0d9ce
MrPaddy35 <paddhole@gmail.com>
2020-07-15 19:27:34 +0530

    Update Hash_Converter_SHA1.py

--- Hash_Converter_SHA1.py
+++ Hash_Converter_SHA1.py
@@ -2,11 +2,9 @@ import requests
 import json
 import time
 
-# check a file of hashes with virustotal and save results
-
-# place your api key
+# place your api key here
 apikey = 'API KEY HERE'
-hashes = open("hash_input.txt") # input with hashes (create a file)
+hashes = open("hash_input.txt") # input with hashes (create a file named hash_input.txt and place your hashes into it.)
 output = open("hash_output.txt", "w") # Output file
 output.write("\t\t\tHash\t\t\t\t\tdetected \n ")
 
@@ -17,7 +15,7 @@ for hashn in hashes:
         response = requests.post('https://www.virustotal.com/vtapi/v2/file/report',
                                 params=params)
         result = response.json()
-	output.write(result['sha1'] + "\n") #can either be MD5 or SHA256
+	output.write(result['sha1'] + "\n") #replace SHA1/MD5/SHA256 to convert existing hashes to the mentioned format.
     except:
         print("API limit reached!, Waiting")
 	time.sleep(1 * 60)
